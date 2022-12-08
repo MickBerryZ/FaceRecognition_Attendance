bb421e8252802f3a789edf5d2b468db1f4e0e107
Thawatchai Sangthep <94940261+MickBerryZ@users.noreply.github.com>
2022-12-08 15:00:22 +0700

    Add files via upload

---
+++ AttendanceProject.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+# from PIL import ImageGrab
+
+path = 'ImagesAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    currentImg = cv2.imread(f'{path}/{cl}')
+    images.append(currentImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def markAttendance(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        # print(myDataList)
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+encodeListKnown = findEncodings(images)
+print(len(encodeListKnown))
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgSmall = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
+
+    facesCurrentFrame = face_recognition.face_locations(imgSmall)
+    encodesCurrentFrame = face_recognition.face_encodings(imgSmall,facesCurrentFrame)
+
+    for encodeFace,faceLocation in zip(encodesCurrentFrame,facesCurrentFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDistance = face_recognition.face_distance(encodeListKnown,encodeFace)
+        print(faceDistance)
+        matchIndex = np.argmin(faceDistance)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1, x2, y2, x1 = faceLocation
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4 # Set for create the green frame to detect face and show output
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            markAttendance(name)
+
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
